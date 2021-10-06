49965b85693ff37badfc80304b01e03d2b5c8932
Agatha Lafaiety <72627509+agathalafaiety@users.noreply.github.com>
2021-10-05 22:28:55 -0300

    Update

--- HelpNow/src/interfaces/iniciais/CadastroCliente.java
+++ HelpNow/src/interfaces/iniciais/CadastroCliente.java
@@ -11,16 +11,17 @@ public class CadastroCliente {
 		
 		String email = null;
 		
-		System.out.print("Digite seu nome: ");
-		String nome = sc.nextLine();
+		System.out.print("\nDigite seu nome: ");
+		String nome = sc.next();
 		
+		System.out.print("Digite seu email: ");
+			email = sc.next();
+			
 		boolean emailValido = false;
 		while(emailValido) {	
-			System.out.println("Digite seu email: ");
-			email = sc.nextLine();
 			for (int i = 0; i < clientes.size(); i++) {
 				if (email.equals(clientes.get(i).getEmail())) {
-					System.out.println("Este email já está cadastrado! \ntente novamente");
+					System.out.println("Este email jÃ¡ estÃ¡ cadastrado! \ntente novamente");
 				}else if(i + 1 == clientes.size()){
 					emailValido = true;
 				}
@@ -29,16 +30,20 @@ public class CadastroCliente {
 			
 		int senha1, senha2;
 		do {
-		System.out.println("Crie uma senha: ");
+		System.out.print("Crie uma senha: ");
 		senha1 = sc.nextInt();
-		System.out.println("Confirme a senha: ");
+		System.out.print("Confirme a senha: ");
 		senha2 = sc.nextInt();
 		if(senha1 != senha2) {
-			System.out.println("As senhas estão diferentes, tente novamente.");
+			System.out.println("\nAs senhas estÃ£o diferentes, tente novamente.\n");
 		}
 		}while(senha1 != senha2);
 		
-		Cliente cliente = new Cliente(nome, email, senha1);	
+		System.out.println("\nCadastro realizado com sucesso!");
+		
+		System.out.println("\nNome: " + nome + "\nEmail de acesso: " + email + "\nSenha de acesso: " + senha2);
+		
+		Cliente cliente = new Cliente(nome, email, senha2);	
 		return cliente;
 	}
 	
